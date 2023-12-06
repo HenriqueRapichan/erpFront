2b2752e66febe338e4a79c5b8bc0582dfb5523dc
HenriqueRapichan <henriquerapichan@gmail.com>
2023-12-05 21:49:06 -0300

    funcoes

---
+++ src/app/components/exemplo/exemplo.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+
+DataService 
+@Component({
+  selector: 'app-exemplo',
+    templateUrl: './exemplo.component.html',
+    styleUrls: ['./exemplo.component.css']
+})
+export class ExemploComponent implements OnInit {
+
+  // Array para armazenar os dados do backend
+  data: any[] = [];
+
+  constructor(private dataService: DataService) {
+    // Carrega dados ao inicializar o componente
+    this.loadData();
+  }
+
+  ngOnInit() {
+  }
+  // Função assíncrona para carregar dados do backend
+  async loadData(): Promise<void> {
+    try {
+      // Chama a função assíncrona do serviço para obter dados
+      this.data = await this.dataService.getData();
+    } catch (error) {
+      // Trata erros ao carregar dados do backend (pode ser exibido uma mensagem para o usuário, por exemplo)
+    }
+  }
+
+  // Função assíncrona para adicionar um novo item e recarregar dados
+  async addItem(newItem: any): Promise<void> {
+    try {
+      // Chama a função assíncrona do serviço para adicionar dados
+      await this.dataService.addData(newItem);
+      // Após adicionar, recarrega os dados para atualizar a interface
+      await this.loadData();
+    } catch (error) {
+      // Trata erros ao adicionar dados (pode ser exibido uma mensagem para o usuário, por exemplo)
+    }
+  }
+
+  // Função assíncrona para editar um item e recarregar dados
+  async editItem(index: number, newItem: any): Promise<void> {
+    try {
+      // Chama a função assíncrona do serviço para editar dados
+      await this.dataService.editData(index, newItem);
+      // Após editar, recarrega os dados para atualizar a interface
+      await this.loadData();
+    } catch (error) {
+      // Trata erros ao editar dados (pode ser exibido uma mensagem para o usuário, por exemplo)
+    }
+  }
+
+  // Função assíncrona para excluir um item e recarregar dados
+  async deleteItem(index: number): Promise<void> {
+    try {
+      // Chama a função assíncrona do serviço para excluir dados
+      await this.dataService.deleteData(index);
+      // Após excluir, recarrega os dados para atualizar a interface
+      await this.loadData();
+    } catch (error) {
+      // Trata erros ao excluir dados (pode ser exibido uma mensagem para o usuário, por exemplo)
+    }
+  }
+}
+
+// Explicações adicionais:
+
+// O componente implementa a interface OnInit, e o método ngOnInit é chamado automaticamente após a inicialização do componente.
+// As funções loadData, addItem, editItem e deleteItem são marcadas como assíncronas (async) porque envolvem operações assíncronas, como chamadas a serviços HTTP.
+// Dentro do bloco try, cada função chama métodos correspondentes do serviço DataService para realizar operações no backend.
+// Dentro do bloco catch, os erros são tratados. No exemplo, uma mensagem de erro é impressa no console, mas você pode personalizar esse comportamento conforme necessário.
+// Certifique-se de substituir as mensagens de erro e adaptar o código de acordo com os requisitos específicos do seu projeto.
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  // URL do backend (substitua 'url_do_backend' pela sua URL real)
+  private apiUrl = 'url_do_backend';
+
+  constructor(private http: HttpClient) {}
+
+  // Função assíncrona para obter dados do backend
+  async getData(): Promise<any[]> {
+    try {
+      // Faz uma requisição GET para obter dados e aguarda a resposta
+      const response = await this.http.get<any[]>(`${this.apiUrl}/dados`).toPromise();
+      return response!; // Retorna os dados obtidos do backend
+    } catch (error) {
+      console.error('Erro ao obter dados do backend', error);
+      throw error; // Lança o erro para que seja tratado pelo componente
+    }
+  }
+
+  // Função assíncrona para adicionar dados no backend
+  async addData(item: any): Promise<void> {
+    try {
+      // Faz uma requisição POST para adicionar dados e aguarda a resposta
+      const response = await this.http.post(`${this.apiUrl}/adicionar`, item).toPromise();
+      console.log('Dados adicionados com sucesso no backend', response);
+    } catch (error) {
+      console.error('Erro ao adicionar dados no backend', error);
+      throw error; // Lança o erro para que seja tratado pelo componente
+    }
+  }
+
+  // Função assíncrona para editar dados no backend
+  async editData(index: number, newItem: any): Promise<void> {
+    try {
+      // Faz uma requisição PUT para editar dados e aguarda a resposta
+      const response = await this.http.put(`${this.apiUrl}/editar/${index}`, newItem).toPromise();
+      console.log('Dados editados com sucesso no backend', response);
+    } catch (error) {
+      console.error('Erro ao editar dados no backend', error);
+      throw error; // Lança o erro para que seja tratado pelo componente
+    }
+  }
+
+  // Função assíncrona para excluir dados no backend
+  async deleteData(index: number): Promise<void> {
+    try {
+      // Faz uma requisição DELETE para excluir dados e aguarda a resposta
+      const response = await this.http.delete(`${this.apiUrl}/excluir/${index}`).toPromise();
+      console.log('Dados excluídos com sucesso no backend', response);
+    } catch (error) {
+      console.error('Erro ao excluir dados no backend', error);
+      throw error; // Lança o erro para que seja tratado pelo componente
+    }
+  }
+}
